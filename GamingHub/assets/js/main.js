399bd12143c630a3b38729a90bb4a1f659e8c4c1
Abdullah Fahd <a.boody.199655@gmail.com>
2021-03-30 17:58:21 +0400

    Update main.js

--- GamingHub/assets/js/main.js
+++ GamingHub/assets/js/main.js
@@ -156,7 +156,7 @@
       var selector = $(this).attr('data-filter');
       $(".portfolio-item").isotope({
         filter: selector ,
-		layoutMode:'masonry'
+		layoutMode:'fitRows'
       })
     });
 
@@ -168,7 +168,7 @@
       var selector = $(this).attr('data-filter');
       $(".portfolio-item").isotope({
         filter: selector ,
-		layoutMode:'masonry'
+		layoutMode:'fitRows'
       })
     });
 
